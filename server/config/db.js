1d7f7a63662d45608f5571dbca4e7d6897169f0c
vkaides <aorata0903@gmail.com>
2020-12-11 20:32:08 -0500

    First commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,5 @@
 module.exports = 
 {
-    //"URI": "mongodb+srv://Iksang:PgzPYWLzSm8sErzL@survey.1d8mb.mongodb.net/survey-list?retryWrites=true&w=majority"
-    //"URI": "mongodb+srv://kinet:eO5KuQfNclODc5mc@mongodbserver.w56bk.mongodb.net/survey-list?retryWrites=true&w=majority"
-    //"URI": "mongodb://localhost/survey_list"
-    "URI": "mongodb+srv://HongViet:aqtDr4CiGSprRZTY@cluster0.jnmc2.mongodb.net/survey_list?retryWrites=true&w=majority"
+    //"URI": " mongodb+srv://HongViet:z0LgZbbbqdTFCNj0@cluster0.8dimu.mongodb.net/survey_list?retryWrites=true&w=majority"
+    "URI": " mongodb+srv://HongViet:YnV33VjFleiCtXAV@cluster0.jnmc2.mongodb.net/survey_list?retryWrites=true&w=majority"
 }
